0840592b36b0d2ca0373a48f8aff9e3377a0c532
iSkinnypink <srbigotexd@gmail.com>
2025-07-16 23:20:26 -0500

    23:20

--- views/menu_principal.py
+++ views/menu_principal.py
@@ -1,34 +1,9 @@
-# from views.vista_registro import mostrar_formulario_registro
-# from views.vista_consulta import mostrar_consulta
-# from views.vista_registro_empleados import modulo_registro_empleados
-
-# def mostrar_menu():
-#     while True:
-#         print("\n=== Menú Principal ===")
-#         print("1. Registrar asistencia")
-#         print("2. Consultar asistencia")
-#         print("3. Registrar empleados")
-#         print("4. Salir")
-
-#         opcion = input("Selecciona una opción: ")
-
-#         if opcion == "1":
-#             mostrar_formulario_registro()
-#         elif opcion == "2":
-#             mostrar_consulta()
-#         elif opcion == "3":
-#             modulo_registro_empleados()
-#         elif opcion == "4":
-#             print("Gracias por usar el sistema. 🖐️")
-#             break
-#         else:
-#             print("❌ Opción inválida.")
-
 from tkinter import *
 #from PIL import Image, ImageTk
 from views.vista_registro import mostrar_formulario_registro
 from views.vista_consulta import mostrar_consulta
 from views.vista_registro_empleados import modulo_registro_empleados
+from modules.consulta_empleados import modulo_consulta_empleados
 from pathlib import Path
 
 def mostrar_menu():
@@ -102,3 +77,4 @@ def mostrar_menu():
            bg="#C0392B", command=ventana.destroy, **style_btn).pack(pady=30)
 
     ventana.mainloop()
+
